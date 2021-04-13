65fd89d7b0fcd115f919ba908b6cecf81c5d951b
Tram Le <jamie.le0308@gmail.com>
2021-04-12 17:58:37 -0700

    Second commit

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Khoa Do on 4/12/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
